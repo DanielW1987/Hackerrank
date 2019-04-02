20df70648d360e5a6c09857cfbe62310a867aef5
DanielW <DanielW@lucanet.com>
2019-04-02 12:58:54 +0200

    Initial commit

---
+++ src/main/java/practice/algorithms/warmup/CompareTriplets.java
@@ -0,0 +1,34 @@
+package practice.algorithms.warmup;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CompareTriplets {
+
+  public static void main(String[] args) {
+    List<Integer> a = Arrays.asList(17, 28, 30);
+    List<Integer> b = Arrays.asList(99, 16, 8);
+
+    List<Integer> result = compareTriplets(a, b);
+    System.out.println(result);
+  }
+
+  private static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+    if (a.size() != b.size()) {
+      throw new IllegalArgumentException("snh: size of 'a' is different than size of 'b'");
+    }
+
+    int pointsOfA = 0;
+    int pointsOfB = 0;
+    for (int index = 0; index < a.size(); index++) {
+      if (a.get(index) > b.get(index)) {
+        pointsOfA++;
+      }
+      else if (a.get(index) < b.get(index)) {
+        pointsOfB++;
+      }
+    }
+
+    return Arrays.asList(pointsOfA, pointsOfB);
+  }
+}
---
+++ src/main/java/practice/algorithms/warmup/PlusMinus.java
@@ -0,0 +1,34 @@
+package practice.algorithms.warmup;
+
+import java.text.DecimalFormat;
+
+public class PlusMinus {
+
+  public static void main(String[] args) {
+    int[] intArray = new int[]{-4, 3, -9, 0, 4, 1};
+    plusMinus(intArray);
+  }
+
+  private static void plusMinus(int[] arr) {
+    double positiveOccurence = 0;
+    double negativeOccurence = 0;
+    double zeroOccurence     = 0;
+
+    for (int number : arr) {
+      if (number > 0) {
+        positiveOccurence++;
+      }
+      else if (number < 0) {
+        negativeOccurence++;
+      }
+      else {
+        zeroOccurence++;
+      }
+    }
+
+    DecimalFormat formatter = new DecimalFormat("#0.######");
+    System.out.println(formatter.format(positiveOccurence / arr.length));
+    System.out.println(formatter.format(negativeOccurence / arr.length));
+    System.out.println(formatter.format(zeroOccurence / arr.length));
+  }
+}
